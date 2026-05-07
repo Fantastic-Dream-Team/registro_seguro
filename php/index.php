f40997c83ba62653d2bc1364d061fde60a2a4f8b
Silver7-7 <bry13espinoza@gmail.com>
2026-05-07 18:44:23 -0500

    update index.php (solo movi unos espacios)

--- php/index.php
+++ php/index.php
@@ -16,22 +16,22 @@
             
             <div class="form-group">
                 <label for="nombre">Nombre completo</label>
-                <input type="text" id="nombre" name="nombre" required 
-                       pattern="[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+" 
-                       title="Solo se permiten letras y espacios">
+                <input type="text" id="nombre" name="nombre" required
+                    pattern="[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+"
+                    title="Solo se permiten letras y espacios">
             </div>
 
             <div class="form-group">
                 <label for="correo">Correo electrónico</label>
-                <input type="email" id="correo" name="correo" required 
-                       placeholder="ejemplo@correo.com">
+                <input type="email" id="correo" name="correo" required
+                    placeholder="ejemplo@correo.com">
             </div>
 
             <div class="form-group">
                 <label for="password">Contraseña</label>
-                <input type="password" id="password" name="password" required 
-                       minlength="8" 
-                       title="Debe contener al menos 8 caracteres">
+                <input type="password" id="password" name="password" required
+                    minlength="8"
+                    title="Debe contener al menos 8 caracteres">
             </div>
 
             <div class="form-group">
